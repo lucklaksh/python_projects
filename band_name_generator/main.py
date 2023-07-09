584f1bcf94764b271e9ec00d59d1b6017a8488b7
Lakshman Kumar <lakshmanlucky821@gmail.com>
2023-07-09 21:49:20 +0530

    Day -1 Band Name Generator

---
+++ band_name_generator/main.py
@@ -0,0 +1,7 @@
+# This project take native palce and pets name as data and suggests a Band name.
+
+print("Welcome to the Band Name Generator.")
+user_name = input("What's name of the city you grew up in?\n")
+pet_name = input ("What's your pet's name?\n")
+
+print("Your band name could be " + user_name + " " + pet_name)
