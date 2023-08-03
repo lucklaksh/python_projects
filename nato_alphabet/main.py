5576b4d2dc1fdb0cc283bbfc41b1e6316479c554
Lakshman Kumar <lakshmanlucky821@gmail.com>
2023-08-03 11:28:05 +0530

    Day 30 Password Manager APP Exception Handling

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -1,18 +1,24 @@
-
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
 import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-# print(data)
-#TODO 1. Create a dictionary in this format:
+
 phonetic_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
 # print(phonetic_alphabet)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-phonetic_list = [phonetic_alphabet[letter] for letter in word if letter in phonetic_alphabet.keys()]
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        phonetic_list = [phonetic_alphabet[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the word please.")
+        generate_phonetic()
+    else:
+        print(phonetic_list)
 
-print(phonetic_list)
+generate_phonetic()
+
+
+
+
+
+
--- password_manager/main.py
+++ password_manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -32,6 +33,12 @@ def save():
     website = website_entry.get().title()
     email = username_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
@@ -41,10 +48,40 @@ def save():
                                        message=f"These are the details entered:\nEmail:{email}\nPassword:{password}\nIs it OK to Save?")
 
     if is_ok:
-        with open("data.txt", mode='a') as data:
-            data.write(f"{website} | {email} | {password}\n")
+        try:
+            with open("data.json", mode='r') as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", mode='w') as data_file:
+                json.dump(new_data, data_file)
+        else:
+            data.update(new_data)
+            with open("data.json", mode='w') as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
+
+# ---------------------------WEBSITE PASSWORD GETTER-----------------------------#
+
+
+def find_password():
+    website = website_entry.get().title()
+    try:
+        with open("data.json", mode='r') as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Erro", message="No Data File Found!")
+    else:
+        if website in data.keys():
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"email: {email}\npassword: {password}")
+        else:
+            messagebox.showinfo(title="Erro", message=f"No Details Of {website} Exists!")
+
+
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -72,9 +109,9 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 # website_entry
-website_entry = Entry(width=40)
+website_entry = Entry(width=25)
 website_entry.focus()
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1)
 
 # username_entry
 username_entry = Entry(width=40)
@@ -82,7 +119,7 @@ username_entry.insert(0, 'lucky@gmail.com')
 username_entry.grid(column=1, row=2, columnspan=2)
 
 # password_entry
-password_entry = Entry(width=22)
+password_entry = Entry(width=25)
 password_entry.grid(column=1, row=3)
 
 # Buttons
@@ -93,5 +130,9 @@ generate_password.grid(column=2, row=3)
 # add button:
 add_button = Button(text="Add", width=35, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
+
+# search button:
+search_button = Button(text="Search", fg="blue", width=15, command=find_password)
+search_button.grid(column=2, row=1)
 
 window.mainloop()
